62ff361cffab34aceac5170ed6dbc6d5c826b066
Akarsh Barar <barar.akarsh8@gmail.com>
2021-06-21 00:23:29 +0530

    First Commit

---
+++ components/Nav.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h4>ReadMe</h4>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Nav />
+    <div></div>
+  </div>
+</template>
+
+<script>
+import Nav from '../components/Nav.vue'
+export default {
+  components: {
+    Nav,
+  },
+  head() {
+    return {
+      title: 'ReadMe: Creative Way to create Readme file.',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
